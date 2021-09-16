d3bf0aabcfbf05b71a5d8ed9fb71a86c0db9521c
littlebabyshit <935396509@qq.com>
2021-09-16 16:23:29 +0800

    添加适配

--- plugin.rb
+++ plugin.rb
@@ -90,5 +90,4 @@ class WechatQRconnectAuthenticator < ::Auth::ManagedAuthenticator
   end
 end
 
-auth_provider title_setting: wechat_qrconnect_title,
-              authenticator: WechatQRconnectAuthenticator.new
+auth_provider authenticator: WechatQRconnectAuthenticator.new
